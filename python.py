cf14249057e49116b279f5fe3c779c2c2e5e54d4
Apostol Adina-Denisa <apostoladina23@stud.ase.ro>
2025-11-17 23:36:16 +0200

    Update python.py

--- python.py
+++ python.py
@@ -3,16 +3,14 @@ import streamlit as st
 st.set_page_config(page_title="Revelion 2025-2026", layout="centered")
 
 # Pink background
-page_bg = """
+st.markdown("""
 <style>
 body {
     background-color: #f7c1d9;
 }
 </style>
-"""
-st.markdown(page_bg, unsafe_allow_html=True)
+""", unsafe_allow_html=True)
 
-# Title
 st.markdown(
     "<h1 style='text-align:center; color:white; text-shadow:2px 2px 4px rgba(0,0,0,0.4);'>"
     "Revelion 2025 - 2026"
@@ -20,15 +18,10 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Marijuana images
-st.image("https://upload.wikimedia.org/wikipedia/commons/5/5f/Cannabis_plant.jpg", width=300)
-st.image("https://upload.wikimedia.org/wikipedia/commons/7/70/Cannabis_flowering.jpg", width=300)
+# One test image
+st.image("https://upload.wikimedia.org/wikipedia/commons/5/5f/Cannabis_plant.jpg", use_column_width=True)
 
-# Button
-clicked = st.button("REVELION 2025-2026 TAP HERE")
-
-# Message after clicking
-if clicked:
+if st.button("REVELION 2025-2026 TAP HERE"):
     st.markdown(
         "<h2 style='color:white; font-weight:bold; text-align:center;'>"
         "SORRY GUYS BUT THIS NEW YEAR WE WILL NOT BE TOGETHER"
