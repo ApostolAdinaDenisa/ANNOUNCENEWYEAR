42680bd95d923b334a776688570b41cd921c7d19
Apostol Adina-Denisa <apostoladina23@stud.ase.ro>
2025-11-17 23:37:19 +0200

    Refactor Streamlit app layout and styling

--- python.py
+++ python.py
@@ -1,29 +1,33 @@
 import streamlit as st
 
-st.set_page_config(page_title="Revelion 2025-2026", layout="centered")
+st.set_page_config(page_title="Revelion 2025-2026")
 
-# Pink background
+# Set full-page pink background (works reliably)
 st.markdown("""
-<style>
-body {
-    background-color: #f7c1d9;
-}
-</style>
+    <style>
+    .stApp {
+        background-color: #f7c1d9 !important;
+        padding-top: 40px;
+    }
+    </style>
 """, unsafe_allow_html=True)
 
+# Title
 st.markdown(
-    "<h1 style='text-align:center; color:white; text-shadow:2px 2px 4px rgba(0,0,0,0.4);'>"
-    "Revelion 2025 - 2026"
-    "</h1>",
+    "<h1 style='text-align:center; color:white;'>Revelion 2025 - 2026</h1>",
     unsafe_allow_html=True
 )
 
-# One test image
-st.image("https://upload.wikimedia.org/wikipedia/commons/5/5f/Cannabis_plant.jpg", use_column_width=True)
+# Images (these always load)
+st.image("https://upload.wikimedia.org/wikipedia/commons/5/5f/Cannabis_plant.jpg", width=300)
+st.image("https://upload.wikimedia.org/wikipedia/commons/7/70/Cannabis_flowering.jpg", width=300)
 
-if st.button("REVELION 2025-2026 TAP HERE"):
+# Button
+clicked = st.button("REVELION 2025-2026 TAP HERE")
+
+if clicked:
     st.markdown(
-        "<h2 style='color:white; font-weight:bold; text-align:center;'>"
+        "<h2 style='text-align:center; color:white; font-weight:bold;'>"
         "SORRY GUYS BUT THIS NEW YEAR WE WILL NOT BE TOGETHER"
         "</h2>",
         unsafe_allow_html=True
